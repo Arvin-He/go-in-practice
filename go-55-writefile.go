403bdb3386341d81cb949cf0c6a995ec7d3b5719
hippo <510205617@qq.com>
2018-11-26 08:52:28 +0800

    update

--- go-55-writefile.go
+++ go-55-writefile.go
@@ -21,9 +21,9 @@ func main () {
 
 	f, err := os.Create("f:/dat2")
 	check(err)
-
+	// 打开文件后，习惯立即使用 defer 调用文件的 Close 操作。
 	defer f.Close()
-
+	// 写入字节切片
 	d2 := []byte{115, 111, 109, 101, 10}
 	n2, err := f.Write(d2)
 	check(err)
@@ -31,12 +31,12 @@ func main () {
 
 	n3, err := f.WriteString("writes\n")
 	fmt.Printf("wrote %d bytes\n", n3)
-
+	// 调用 Sync 来将缓冲区的信息写入磁盘。
 	f.Sync()
-
+	// bufio 提供了和我们前面看到的带缓冲的读取器一样的带缓冲的写入器。
 	w := bufio.NewWriter(f)
 	n4, err := w.WriteString("buffered\n")
 	fmt.Printf("wrote %d bytes\n", n4)
-
+	// 使用 Flush 来确保所有缓存的操作已写入底层写入器。
 	w.Flush()
 }
